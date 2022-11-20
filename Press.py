14eac4e1fb8f01d73f0cbdae1b434657725ab617
Software Engineer <a.benraies19038@pi.tn>
2022-11-20 14:22:58 +0100

    Second commit

--- Press.py
+++ Press.py
@@ -8,16 +8,17 @@ def login():
     Password="password"
     K.FAILSAFE=False
     K.hotkey('ctrl', 'alt', 'o')
-    sleep(5)
-    for letter in site:
-        K.press(letter,presses=1)
+    sleep(4)
+    K.write('facebook')
     K.press("enter")
-    sleep(1.5)
-    for letter in username:
-        K.press(letter,presses=1)
+    K.sleep(3)
+    K.write(username)
     K.press("tab")
-    for letter in Password:
-        K.press(letter,presses=1)
+    K.write(Password)
     K.press("enter")
-                            
-
+    # for letter in username:
+    #     K.press(letter,presses=1)
+    # K.press("tab")
+    # for letter in Password:
+    #     K.press(letter,presses=1)
+    # K.press("enter")
--- main.py
+++ main.py
@@ -5,16 +5,6 @@ import math
 import numpy as np 
 from Press import login 
 
-def face_confidence(face_distance,face_match_threshold=0.6):
-    range=(1.0-face_match_threshold)
-    linear_val=(1.0-face_distance)/(range*2.0)
-
-    if face_distance > face_match_threshold:
-        return str(round(linear_val*100,2))+"%"
-    else: 
-        value=(linear_val+((1.0-linear_val) * math.pow((linear_val-0.5),0.2)))*100
-        return str(round(value,2))+"%"
-
 
 
 class ReconnaissanceFaciale():
@@ -30,24 +20,34 @@ class ReconnaissanceFaciale():
     def __init__(self):
         pass
 
+    #métriques de performance
+    def face_confidence(self,face_distance,face_match_threshold=0.6):
+        range=(1.0-face_match_threshold)
+        linear_val=(1.0-face_distance)/(range*2.0)
+
+        if face_distance > face_match_threshold:
+            return str(round(linear_val*100,2))+"%"
+        else: 
+            value=(linear_val+((1.0-linear_val) * math.pow((linear_val-0.5),0.2)))*100
+            return str(round(value,2))+"%"
+
+
+    #collection des donnes et leurs preparations(encodage) !
     def face_encoding(self):
         for image in os.listdir("Faces"):
             face_image=face_recognition.load_image_file(f'Faces/{image}')
-            #print(face_image)
+            #etant donné une image , retourne un vecteur de 128 dimension 
+            #Pour une image donné le vecteur de 128 dimension est UNIQUE !
             face_encoding=face_recognition.face_encodings(face_image)[0]
-            #print(face_encoding)
             self.known_faces_encodings.append(face_encoding)
             self.known_faces_names.append(image)
-        print("These are the known faces and its encodings !")
-        # print(self.known_faces_encodings)
-        # print(self.known_faces_names)
+    
 
-
+    #fonction de reconnaissance : 
     def run_recognition(self):
         video_capture=cv.VideoCapture(0)
 
         if not video_capture.isOpened():
-            
             sys.exit("Video source not found !")
 
         while True :
@@ -68,26 +68,33 @@ class ReconnaissanceFaciale():
                     best_match_index=np.argmin(face_distances)
                     if matches[best_match_index]:
                         name=self.known_faces_names[best_match_index]
-                        confidence=face_confidence(face_distances[best_match_index])
-                        if name=="anas ben raies.jpg":
-                            login()
-                            return True
+                        confidence=self.face_confidence(face_distances[best_match_index])
+                        # if name=="anas ben raies.jpg":
+                        #     login()
+                        #     return True
                     self.face_names.append(f'{name} ({confidence})')
             self.process_current_frame=not self.process_current_frame
             for (top ,right ,bottom,left) , name in zip(self.face_locations,self.face_names):
-                top*=4
-                right*=4
-                bottom*=4
-                left*=4
+                    top*=4
+                    right*=4
+                    bottom*=4
+                    left*=4
+                
+                    if "Unknown" in name:
+                        cv.rectangle(frame,(left,top),(right,bottom),(0,0,255),2)
+                        cv.rectangle(frame,(left,bottom-25),(right,bottom),(0,0,255),-1)
+                    else:
+                        cv.rectangle(frame,(left,top),(right,bottom),(0,255,0),2)
+                        cv.rectangle(frame,(left,bottom-25),(right,bottom),(0,255,0),-1)
+                    cv.putText(frame,name,(left+6,bottom-6),cv.FONT_HERSHEY_DUPLEX,0.8,(255,255,255),1)
 
-                cv.rectangle(frame,(left,top),(right,bottom),(0,0,255),2)
-                cv.rectangle(frame,(left,bottom-25),(right,bottom),(0,0,255),-1)
-                cv.putText(frame,name,(left+6,bottom-6),cv.FONT_HERSHEY_DUPLEX,0.8,(255,255,255),1)
+                
+                
 
             cv.imshow("Face Recognition",frame)
             key=cv.waitKey(1)
             
-            #aaa
+            
             if key == 27 :
                 break
         video_capture.release()
